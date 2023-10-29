d4813f39775f2718805029cc29ceafeb37329c1d
Alexandre <rodenas.alexandre@gmail.com>
2023-10-29 17:40:07 +0100

    Store blurr level in dataframe

--- src/pixel_analysis.py
+++ src/pixel_analysis.py
@@ -14,19 +14,13 @@ class PixelAnalysis:
         self.num_cores = config.pixel_analysis.num_cores
         self.blurriness_threshold = config.pixel_analysis.blurriness_threshold
 
-    @staticmethod
-    def _get_white_percentage(image_content):
-        image = cv2.imdecode(np.frombuffer(image_content, np.uint8), cv2.IMREAD_GRAYSCALE)
-        n_white_pix = np.sum(image == 255)
-        return int((n_white_pix / image.size) * 100)
-
     def enriched(self, images_dataframe):
         logging.info("Computing percentage of white in images")
 
         def process_image(row):
             try:
                 white = self._get_white_percentage(row["content"])
-                blur = self._get_blur_percentage(row['content'])
+                blur = self._get_blur_level(row['content'])
                 return white, blur
             except Exception as e:
                 logging.error(f"An exception occurred with {row['path']}: {str(e)}")
@@ -42,11 +36,18 @@ class PixelAnalysis:
         else:
             logging.warning("White analysis and image DataFrame lengths do not match.")
 
-    def _get_blur_percentage(self, image_content):
+    @staticmethod
+    def _get_white_percentage(image_content):
+        image = cv2.imdecode(np.frombuffer(image_content, np.uint8), cv2.IMREAD_GRAYSCALE)
+        n_white_pix = np.sum(image == 255)
+        return int((n_white_pix / image.size) * 100)
+
+    @staticmethod
+    def _get_blur_level(image_content):
         try:
             image = cv2.imdecode(np.frombuffer(image_content, np.uint8), cv2.IMREAD_GRAYSCALE)
             laplacian_var = cv2.Laplacian(image, cv2.CV_64F).var()
-            return laplacian_var < self.blurriness_threshold
+            return int(laplacian_var)
 
         except Exception as e:
             print(f"Error analyzing image: {str(e)}")
